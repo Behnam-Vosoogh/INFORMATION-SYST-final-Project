5104223ae5bd2fda3a6e79e2a1f962919f4bdd67
Edw-lo <edwardkinglv@gmail.com>
2021-04-14 19:22:42 -0400

    Codename Project

---
+++ finalProject/src/finalProject/Card.java
@@ -0,0 +1,18 @@
+package finalProject;
+
+public class Card {
+	String colors;
+	Character first;
+	
+	public Card()
+	{
+		
+	}
+	
+	public Card(String inColor, Character inFirst)
+	{
+		this.colors = inColor;
+		this.first = inFirst;
+	}
+
+}
---
+++ finalProject/src/finalProject/Pair.java
@@ -0,0 +1,17 @@
+package finalProject;
+
+public class Pair {
+	Boolean used;
+	  
+	  String word;
+	  
+	  public Pair(String inWord) {
+	    this.used = Boolean.valueOf(false);
+	    this.word = inWord;
+	  }
+	  
+	  public Pair() {
+		  
+	  }
+
+}
